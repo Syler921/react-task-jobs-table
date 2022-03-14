428142b2b5a0979014ad0ffa3981727d8d88cd88
syler921 <syler921@gmail.com>
2022-03-14 11:15:04 +0200

    test

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -6,6 +6,7 @@ import SearchBar from '../SearchBar/SearchBar'
 class Table extends React.Component {
     constructor(props) {
         super(props);
+        console.log('test')
     }
 
     onSubmit = (formValues) => {
